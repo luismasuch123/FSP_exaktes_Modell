67bfc4f826c005c4fe78f8558c60b8b6fd347f10
luismasuchibanez@googlemail.com <Maluni1894>
2022-03-03 12:38:06 +0100

    plot_sol und sol_checker Zugriff auf x angepasst

--- fsp_solution_checker.py
+++ fsp_solution_checker.py
@@ -11,7 +11,7 @@ import os
 from fsp_instance_checker import skillsOfRequiredLevel
 
 
-def check_solution(k_set, i_set, in_set, s_set, l_set, d_k_s, d_k_e, r_i_s_l, s_k_s_l, y_in, z_k_in, x_i_j_k, path):
+def check_solution(k_set, i_set, j_set, in_set, s_set, l_set, d_k_s, d_k_e, r_i_s_l, s_k_s_l, y_in, z_k_in, x_i_j_k, path):
     with open(path + "/Solution_Checker.txt", "w") as datei:
 
         text = "\n ---Lösungschecker--- \n"
@@ -21,12 +21,12 @@ def check_solution(k_set, i_set, in_set, s_set, l_set, d_k_s, d_k_e, r_i_s_l, s_
         text = "Fahren Techniker los? \n"
         print(text)
         datei.write(text)
-        do_technicians_leave(k_set, i_set, x_i_j_k, d_k_s, datei)
+        do_technicians_leave(k_set, i_set, j_set, x_i_j_k, d_k_s, datei)
 
         text = "\nKommen Techniker zum Depot zurück? \n"
         print(text)
         datei.write(text)
-        do_technicians_come_back(k_set, i_set, x_i_j_k, d_k_e, datei)
+        do_technicians_come_back(k_set, i_set, j_set, x_i_j_k, d_k_e, datei)
 
         text = "\nFahren Techniker los ohne einen Task zu erfüllen? \n"
         print(text)
@@ -35,7 +35,7 @@ def check_solution(k_set, i_set, in_set, s_set, l_set, d_k_s, d_k_e, r_i_s_l, s_
 
         text = "\nWerden Tasks nicht erfüllt, obwohl die dafür benötigten Skills und Level prinzipiell vorhanden wären? \n"
         print(text)
-        datei .write(text)
+        datei.write(text)
         do_realisable_tasks_get_fulfilled(in_set, y_in, datei)
 
         text = "\nSind alle Techniker, die dem Team zugewiesen sind, um einen Task zu erfüllen, qualifiziert? \n"
@@ -44,11 +44,11 @@ def check_solution(k_set, i_set, in_set, s_set, l_set, d_k_s, d_k_e, r_i_s_l, s_
         are_members_of_teams_qualified(in_set, s_set, l_set, k_set, r_i_s_l, s_k_s_l, y_in, z_k_in, datei)
 
 
-def do_technicians_leave(k_set, i_set, x_i_j_k, d_k_s, datei):
+def do_technicians_leave(k_set, i_set, j_set, x_i_j_k, d_k_s, datei):
     allLeave = True
     for k in k_set:
         leftDepotCount = 0
-        for j in i_set:
+        for j in set(j_set[d_k_s[k]]):
             x = x_i_j_k[d_k_s[k], j, k].X
             if x > 0.9 and x < 1:
                 x = 1
@@ -68,15 +68,16 @@ def do_technicians_leave(k_set, i_set, x_i_j_k, d_k_s, datei):
         datei.write(text)
 
 
-def do_technicians_come_back(k_set, i_set, x_i_j_k, d_k_e, datei):
+def do_technicians_come_back(k_set, i_set, j_set, x_i_j_k, d_k_e, datei):
     allComeBack = True
     for k in k_set:
         ComeBackToDepotCount = 0
         for i in i_set:
-            x = x_i_j_k[i, d_k_e[k], k].X
-            if x > 0.9 and x < 1:
-                x = 1
-            ComeBackToDepotCount += x
+            if i != d_k_e[k]:
+                x = x_i_j_k[i, d_k_e[k], k].X
+                if x > 0.9 and x < 1:
+                    x = 1
+                ComeBackToDepotCount += x
         if ComeBackToDepotCount < 1:
             text = "Techniker " + str(k + 1) + " kehrt nicht zum Depot zurück! \n"
             print(text)
@@ -110,11 +111,17 @@ def do_technicians_fulfill_task(k_set, in_set, z_k_in, datei):
 
 
 def do_realisable_tasks_get_fulfilled(in_set, y_in, datei):
+    allRealisableTasksFulfilled = True
     for i in in_set:
         if y_in[i].X != skillsOfRequiredLevel[i]:
             text = "Task " + str(i + 1) + " wurde nicht erfüllt, obwohl die notwendigen Qualifikationen vorhanden wären! \n"
+            allRealisableTasksFulfilled = False
             print(text)
             datei.write(text)
+    if allRealisableTasksFulfilled:
+        text = "Alle Tasks, für welche die notwendigen Qualifikationen vorhanden sind, werden erfüllt! \n"
+        print(text)
+        datei.write(text)
 
 
 def are_members_of_teams_qualified(in_set, s_set, l_set, k_set, r_i_s_l, s_k_s_l, y_in, z_k_in, datei):
